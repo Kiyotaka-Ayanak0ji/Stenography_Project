3b6c6c4a12aec274a2ff49016195d4481147a66d
root <root@LAPTOP-VK7G0RMK>
2025-03-11 14:44:14 +0530

    Added Dark mode support along with navbar animations and transitions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     
-    const [isDark,setIsDark] = useState(false);
+    const [isDark,setIsDark] = useState(() => {
+        const theme = localStorage.getItem("isDark");
+
+        //Default light theme...
+        return theme ? JSON.parse(theme) : false;
+    })
     
     const navigate = useNavigate();
 
     const handleDark = () => {
+
+        const new_state = !isDark;
+        
+        //Update the state...
+        setIsDark(new_state); 
+
+        //Update the state in the local storage...
+        localStorage.setItem("isDark",JSON.stringify(new_state));
+    }
+
+    useEffect(() => {
         if(isDark){
             //Select the root document element...
             document.documentElement.classList.remove("dark");
@@ -18,44 +34,49 @@ const Navbar = () => {
             document.documentElement.classList.remove("light");
             document.documentElement.classList.add("dark");
         }
-        
-        //Update the state...
-        setIsDark(!isDark);
-    }
+    },[isDark]);
 
     if(isDark){
         return(
-            <div className='h-15 flex flex-row w-full bg-stone-900 dark:bg-slate-200 justify-end'>
-            <button onClick={() => navigate('/encrypt')} className='underline-effect flex h-full px-1 py-1 transition ease-linear text-white dark:text-stone-900 hover:bg-white hover:text-stone-950 dark:hover:bg-slate-950 dark:hover:text-white'>
-                Encrypt
-            </button>
-            <button onClick={() => navigate('/decrypt')} className='underline-effect flex h-full px-1 py-1 transition ease-linear text-white dark:text-stone-900 hover:bg-white hover:text-stone-950 dark:hover:bg-slate-950 dark:hover:text-white'>
-                Decrypt
-            </button>
-            <button className='flex' onClick={() => handleDark}>
-                {/* Sun icon */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 animate-sun">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                </svg>            
-            </button>
-        </div>    
+            <div className='h-20 flex flex-row w-full bg-stone-900 justify-between items-center pl-3 pr-3'>
+                <div className="flex justify-start">
+                    <button className='flex' onClick={handleDark}>
+                        {/* Sun icon */}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="opacity-60 size-16 hover:opacity-100 transition ease-linear animate-sun">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                        </svg>            
+                    </button>
+                </div>
+                <div className="flex flex-row justify-end gap-10">
+                    <button onClick={() => navigate('/encrypt')} className='underline-effect flex text-3xl h-full px-1 py-1 transition ease-linear hover:text-stone-950 hover:bg-slate-100 text-white'>
+                        Encrypt
+                    </button>
+                    <button onClick={() => navigate('/decrypt')} className='underline-effect flex h-full text-3xl px-1 py-1 transition ease-linear hover:text-stone-950 hover:bg-slate-100 text-white'>
+                        Decrypt
+                    </button>
+                </div>
+            </div>    
         )
     }
     else{
         return (
-            <div className='h-15 flex flex-row w-full bg-stone-900 dark:bg-slate-200 justify-end'>
-                <button onClick={() => navigate('/encrypt')} className='flex h-full px-1 py-1 transition ease-linear text-white dark:text-stone-900 hover:bg-white hover:text-stone-950 dark:hover:bg-slate-950 dark:hover:text-white'>
-                    Encrypt
-                </button>
-                <button onClick={() => navigate('/decrypt')} className='flex h-full px-1 py-1 transition ease-linear text-white dark:text-stone-900 hover:bg-white hover:text-stone-950 dark:hover:bg-slate-950 dark:hover:text-white'>
-                    Decrypt
-                </button>
-                <button className='flex' onClick={() => handleDark}>
-                    {/* Moon*/}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 animate-moon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                    </svg>
-                </button>
+            <div className='h-20 flex flex-row w-full bg-slate-200 justify-between items-center pl-3 pr-3'>
+                <div className="flex justify-start">
+                    <button className='flex' onClick={handleDark}>
+                        {/* Moon*/}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-16 opacity-60 hover:opacity-100 animate-moon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                        </svg>
+                    </button>
+                </div>
+                <div className="flex flex-row justify-end gap-10">
+                    <button onClick={() => navigate('/encrypt')} className='text-3xl underline-effect flex h-full px-1 py-1 transition ease-linear hover:text-slate-100 hover:bg-stone-950 text-stone-950'>
+                        Encrypt
+                    </button>
+                    <button onClick={() => navigate('/decrypt')} className='text-3xl underline-effect flex h-full px-1 py-1 transition ease-linear hover:text-slate-100 hover:bg-stone-950 text-stone-950'>
+                        Decrypt
+                    </button>
+                </div>
             </div>
         )
     }
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div className='flex flex-col gap-2 h-full w-full items-center justify-center'>
-        <div className='h-20 w-full justify-center'>
+        <div className='w-full justify-center'>
             <Navbar/>
         </div>
         <div className='w-full text-center justify-center items-center'>
-            <p className='text-neutral-800 text-4xl'>
+            <p className='text-slate-200 text-5xl pt-5'>
                 Stenography Online
             </p>
         </div>
